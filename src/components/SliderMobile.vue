b33127704c03fafd37da1b6070ec1cfcba111448
olegs <olegsirota95@gmail.com>
2019-09-07 16:49:26 +0300

    fix slider transform

--- src/components/SliderMobile.vue
+++ src/components/SliderMobile.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class>
-		<h1>Custom Select</h1>
-		<div class="list-wrap" v-hammer:swipe="onPan" ref="list">
+		<h1>Custom Slider</h1>
+		<div class="list-wrap" v-hammer:swipe="onPan" ref="list" :style="{ width: widthWrapper }">
 			<div class="list-element" v-for="item in list" :key="item.id">
 				<figure class="image">
 					<img :src="item.img" alt="Image" />
@@ -27,36 +27,32 @@ export default {
 	},
 	data() {
 		return {
-			currentNumber: 0,
 			activeSlide: 0,
-			timer: null
 		};
 	},
 	computed: {
-		overflowRatio() {
-			return this.$refs.list.scrollWidth / this.$refs.list.offsetWidth;
-		},
-		itemWidth() {
-			return this.$refs.list.scrollWidth / this.list.length;
-		},
-
 		count() {
 			return this.list.length;
-		}
+		},
 	},
 	methods: {
 		onPan(e) {
-			const percentage = ((100 / this.count) * e.deltaX) / this.itemWidth;
-			const transform = -this.itemWidth;
-			this.$refs.list.style.transform = "translateX( " + transform + "% )";
+			const percentage = ((100 / this.count) * e.deltaX) / window.innerWidth;
+			const transform = percentage - 100 / this.count * this.activeSlide;
+			this.$refs.list.style.transform = `translateX(${transform }%)`;
 			if (e.isFinal) {
 				if (percentage < 0) {
 					this.goToSlide(this.activeSlide + 1);
-				} else if (percentage > 0) this.goToSlide(this.activeSlide - 1);
-				else this.goToSlide(this.activeSlide);
+				} else if (percentage > 0){
+					this.goToSlide(this.activeSlide - 1);
+				} else {
+					this.goToSlide(this.activeSlide);
+				}
 			}
 		},
-
+		widthWrapper() {
+			this.$refs.list.style.width = `${this.list.length * 100}%`;
+		},
 		goToSlide(number) {
 			if (number < 0) {
 				this.activeSlide = 0;
@@ -65,12 +61,16 @@ export default {
 			} else {
 				this.activeSlide = number;
 			}
-			var percentage = -(12 * (this.count * this.activeSlide));
-			this.$refs.list.style.transform = "translateX(" + percentage + "%)";
+			let percentage = -(100 / this.count )* this.activeSlide;
+			this.$refs.list.style.transform = `translateX(${percentage}%)`;
 		}
+	},
+	mounted() {
+		this.widthWrapper();
 	}
 };
 </script>
+
 <style lang="scss" scoped>
 .list-wrap {
 	display: grid;
@@ -94,7 +94,6 @@ export default {
 		font-size: 2rem;
 		backface-visibility: hidden;
 		.list-element {
-			flex: 0 0 80%;
 			margin: 0 20px;
 			box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 			padding: 16px;
